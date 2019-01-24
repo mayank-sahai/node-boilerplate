b5d5a23a2ece597c78891a5e28db5715966c0b33
oshin18 <oshin18nov@gmail.com>
2019-01-24 11:02:26 +0530

    environment setup and minor improvements

--- server.js
+++ server.js
@@ -2,26 +2,27 @@
 
 /************************ Require Node modules **********************/
 const express = require('express'),
-bodyParser = require('body-parser'),
-config = require('config');
+  bodyParser = require('body-parser'),
+  config = require('config');
 
 /************************ Require Local modules **********************/
 const routers = require('./routes');
 
 /************************ Varaiable Listing **********************/
 const app = express(),
-router = express.Router(),
-port = config.get('General').port;
+  router = express.Router(),
+  port = config.get('General').port,
+  env = process.env.NODE_ENV || 'development';
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api', router);
 routers(router);
 
-app.listen(port , (error, result) => {
-  if (error) 
+app.listen(port, (error, result) => {
+  if (error)
     console.log("Error while Application startup", error);
-  else 
-    console.log("Applcation connected at port:", port);
+  else
+    console.log(`Application connected to ${env} environment at ${port} port`);
 });
 
