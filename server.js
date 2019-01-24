516c9d06fc998fabeed234825fdb742215c85b41
oshin18 <oshin18nov@gmail.com>
2019-01-24 12:26:35 +0530

    Logger implementation

--- server.js
+++ server.js
@@ -1,35 +1,27 @@
 
-
 /** ********************** Require Node modules ********************* */
 const express = require('express');
-
-
 const bodyParser = require('body-parser');
-
-
 const config = require('config');
 
 /** ********************** Require Local modules ********************* */
 const routers = require('./routes');
+const logger = require('./utils/logger');
 
 /** ********************** Varaiable Listing ********************* */
 const app = express();
-
-
 const router = express.Router();
-
-
 const { port } = config.get('General');
-
-
 const env = process.env.NODE_ENV || 'development';
 
+// Router Setup
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api', router);
 routers(router);
 
+// Server Start
 app.listen(port, (error) => {
-  if (error) console.log('Error while Application startup', error);
-  else console.log(`Application connected to ${env} environment at ${port} port`);
+  if (error) logger.error('Error while Application startup', error);
+  else logger.info(`Application connected to ${env} environment at ${port} port`);
 });
