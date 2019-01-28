b4a376aadb8f664b70ed59664c505fef59fdbeac
mayank-sahai <mayank.ram22@gmail.com>
2019-01-28 19:39:14 +0530

    Csurf implementation

--- routes.js
+++ routes.js
@@ -1,7 +1,11 @@
 
 
 module.exports = (app) => {
-  app.get('/check', (req, res) => {
+  app.get('/', (req, res) => {
+    res.send({ status: true, message: req.csrfToken() });
+  });
+
+  app.post('/check', (req, res) => {
     res.send({ status: true, message: 'Ok' });
   });
 };
--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('config');
+const csrf = require('csurf');
+const session = require('express-session');
 const swaggerUi = require('swagger-ui-express');
 
 /** ********************** Require Local modules ********************* */
@@ -13,23 +15,31 @@ const swaggerDocument = require('./swagger.json');
 /** ********************** Varaiable Listing ********************* */
 const app = express();
 const router = express.Router();
-const { port } = config.get('General');
+const { port, csrfKey } = config.get('General');
 const env = process.env.NODE_ENV || 'development';
+const csrfProtection = csrf({ cookie: false });
 
 // Router Setup
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({
+  secret: csrfKey,
+}));
+app.use(csrfProtection);
+
 app.use('/api', router);
+
 routers(router);
 
 // Swagger configuration
 if (env === 'development') {
-  var options = {
-    explorer : true
+  const options = {
+    explorer: true,
   };
-   
+
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
-  logger.info(`Swagger running on http://localhost:5000/api-docs`);
+  logger.info('Swagger running on http://localhost:5000/api-docs');
 }
 
 // Server Start
